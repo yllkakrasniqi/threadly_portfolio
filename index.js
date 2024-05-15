0429ac415598013f0bff86a086f03e3baeb6ecf5
Yllka Krasniqi <yllkakrasniqi98@hotmail.com>
2024-05-15 21:57:57 +0200

    Setup a basic project

---
+++ index.js
@@ -0,0 +1,29 @@
+const express = require("express");
+const cors = require("cors");
+const cookieSession = require("cookie-session");
+
+const PORT = 3003;
+const app = express();
+app.use(
+  cors({
+    origin: "http://localhost:3000", // Replace with your client's origin
+    credentials: true,
+  })
+);
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(
+  cookieSession({
+    name: "auth-session",
+    secret: "COOKIE_SECRET",
+    httpOnly: true,
+  })
+);
+
+app.get("/", (req, res) => {
+    res.send("Hello world\n");
+});
+
+app.listen(PORT, () => {
+    console.log(`Server listening on ${PORT}`);
+});
