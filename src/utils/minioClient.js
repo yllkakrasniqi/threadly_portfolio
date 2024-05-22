bcf8b15ef71614b25203179f4e0987fc197a4886
Yllka Krasniqi <yllkakrasniqi98@hotmail.com>
2024-05-22 15:00:36 +0200

    Fix uploading files

--- src/utils/minioClient.js
+++ src/utils/minioClient.js
@@ -4,6 +4,6 @@ exports.minioClient = new Minio.Client({
     endPoint: "127.0.0.1",
     port: 9000,
     useSSL: false,
-    accessKey: "rTEo84NtDCTjS0mRot9e",
-    secretKey: "G7hlqpG1zTb6iBlc5EJvaGXox4btueMTbTGOkzGF",
+    accessKey: "uXNrVuUHChgW999eZp6M",
+    secretKey: "VwShqcoVbz6QHvTdKN8YM2YupkEazoJk560Lx80q",
 });
